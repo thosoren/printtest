8c4e53197c20892b07a052ec86d79b22098e0a5c
Thomas <thosoren@gmail.com>
2019-12-13 22:53:39 +0100

    second commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const jQuery = require('jquery');
+
+
+//Set ENV
+process.env.NODE_ENV = 'development';
+ 
+ const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+ let mainWindow;
+
+ app.on('ready',function() {
+ 	//Create new window
+ 	mainWindow = new BrowserWindow({
+ 		webPreferences: {
+            nodeIntegration: true
+        }
+ 	});
+ 	//Load html into window
+ 	mainWindow.loadURL(url.format({
+ 		pathname: path.join(__dirname,'mainWindow.html'),
+ 		protocol:'file',
+ 		slashes: true
+ 	}));
+
+ 	mainWindow.on('closed', function() {
+ 		app.quit();
+ 	});
+
+ 	mainWindow.webContents.openDevTools()
+ });
+
+ 
+/*printer
+  .list()
+  .then(console.log)
+  .catch(console.error);*/
+
+/*printer
+  .print("pdf-sample.pdf")
+  .then(console.log)
+  .catch(console.error);
+  */
